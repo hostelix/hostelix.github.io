0390d8cd6e84d4e4af752d5ede74ac053b8d4ec1
Israel Lugo <hostelixisrael@gmail.com>
2017-08-08 10:03:50 -0400

    se cambio el selector de GitHubCalendar

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "hostelix");
+    GitHubCalendar(".github-graph", "hostelix");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
